8a71188126bb3dec7b897ff14ff1dea5ee3f7ee7
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2023-07-25 21:47:36 +0200

    PaperColor theme

--- configs/nvim/lua/plugins/misc.lua
+++ configs/nvim/lua/plugins/misc.lua
@@ -5,12 +5,12 @@ return {
   { "folke/neoconf.nvim", cmd = "Neoconf" },
 
   {
-    "folke/tokyonight.nvim",
+    "NLKNguyen/papercolor-theme",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.cmd([[colorscheme PaperColor]])
     end,
   },
 }
