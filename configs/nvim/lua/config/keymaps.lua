08e8b03437837d8d3a65c0a501a2d724c5aab738
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2023-07-25 21:46:58 +0200

    diagnostic keymaps

--- configs/nvim/lua/config/keymaps.lua
+++ configs/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 
 vim.keymap.set('n', '<leader>ff', ':NvimTreeFindFileToggle<CR>', { desc = '[f]ind [f]ile' })
 
+vim.keymap.set('n', '<leader>dn', ':lua vim.diagnostic.goto_next()<CR>', { desc = 'next diagnostic' })
+vim.keymap.set('n', '<leader>dp', ':lua vim.diagnostic.goto_prev()<CR>', { desc = 'previous diagnostic' })
+vim.keymap.set('n', '<leader>do', ':lua vim.diagnostic.open_float()<CR>', { desc = 'open diagnostic' })
+
