4da5c2164e1563933cd9e58bb1c9fd63b8833086
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2024-06-18 08:08:32 +0200

    new nvim config

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,2 +1,85 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { 
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      term_colors = true,
+      transparent_background = false,
+      styles = {
+        comments = {},
+        conditionals = {},
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+      },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+            enabled = true,
+            style = "nvchad",
+        },
+        dropbar = {
+            enabled = true,
+            color_mode = true,
+        },
+      },
+    },
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
+  }
+}
+
+local opts = {
+	install = {
+	},
+}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+vim.cmd.colorscheme "catppuccin"
+
