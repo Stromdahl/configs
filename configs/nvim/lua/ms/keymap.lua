3b9d1ad3d2b06f27f64510840f6d5d417d201b7f
Mattias Strömdahl <mattias.stromdahl@gmail.com>
2025-04-29 10:32:39 +0200

    Add keymap

--- configs/nvim/lua/ms/keymap.lua
+++ configs/nvim/lua/ms/keymap.lua
@@ -20,10 +20,11 @@ M.oil = {
 M.fzf_lua  = {
     { '<leader><leader>', function() require('fzf-lua').files() end, desc="[f]ind [f]file" },
     { '<leader>ff', function() require('fzf-lua').files() end, desc="[f]ind [f]file"},
-    { '<leader>fc', function() require('fzf-lua').files() end, desc="[f]ind [f]file"},
+    { '<leader>fc', function() require('fzf-lua').files({cwd = '%:p:h'}) end, desc="[f]ind [f]file"},
     { '<leader>fg', function() require('fzf-lua').live_grep() end, desc="[[f]ind files by [g]rep]"},
     { '<leader>fb', function() require('fzf-lua').buffers() end, desc="[[f]ind in [b]uffer]"},
     { '<leader>/', function() require('fzf-lua').grep_curbuf() end, desc="Grep in current buffer"}
 }
 
+-- vim.fn.expand('%:p') 
 return M
